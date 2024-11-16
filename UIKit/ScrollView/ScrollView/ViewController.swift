ba374e93bfeb298f06896a5fb244659f882c2a57
E-know <eknow25@gmail.com>
2024-11-16 18:35:58 +0900

    CollectionView 스크롤 안하기

---
+++ UIKit/ScrollView/ScrollView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Inho Choi on 12/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func tapped(_ sender: Any) {
+        print("TAPP")
+    }
+}
+
